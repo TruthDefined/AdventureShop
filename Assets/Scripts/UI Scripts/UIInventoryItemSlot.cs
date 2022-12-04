38fc82b54e88ba3ac1aa2b73839270a79fa9c469
RJ Perry <raymond.perry@maine.edu>
2022-12-04 03:30:45 -0500

    Inventory shows icons

--- Assets/Scripts/UI Scripts/UIInventoryItemSlot.cs
+++ Assets/Scripts/UI Scripts/UIInventoryItemSlot.cs
@@ -1,31 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIInventoryItemSlot : MonoBehaviour
 {
-    private Sprite m_icon;
+    public Image iconImage;
+    public GameObject stackLabel;    
     private TMP_Text m_label;
-    public GameObject m_stackObject;
-    private TMP_Text m_stackLabel;
 
     private void Awake() {
-        //m_icon = GetComponent<Sprite>();
         m_label = GetComponent<TMP_Text>();
-        m_stackLabel = m_stackObject.GetComponent<TMP_Text>();
     }
-
-
     public void Set(InventoryItem item){
-        //m_icon = item.data.icon;
+        iconImage.sprite = item.data.icon;
         m_label.text = item.data.name;
         // if(item.stackSize <= 0){
         //     m_stackObject.SetActive(false);
         //     return;
         // }
-        m_stackLabel.text = item.stackSize.ToString();
+        stackLabel.GetComponent<TMP_Text>().text = item.stackSize.ToString();
     }
+
+    
 
     public void Add(){
         var temp = Singleton.Instance.Player_Raw_Inventory.inventory.Find((x) => x.data.name == m_label.text);
